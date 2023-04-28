18c1fe48c5c0434d83b53f20a0b1acf203c2ac07
walwyde <walwyde@gmail.com>
2023-04-29 00:15:59 +0100

    more code on Profile component

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,27 +1,52 @@
-import React, {useEffect} from 'react'
-import PropTypes from 'prop-types'
-import { getProfileById } from '../../actions/profile'
-import {connect} from 'react-redux'
+import React, { Fragment, useEffect } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import { Spinner } from "../layouts/Spinner";
+import { getProfileById } from "../../actions/profile";
+import { connect } from "react-redux";
 
-const Profile = ({match, getProfileById}) => {
-  console.log(match.params.id)
+const Profile = ({
+  match,
+  getProfileById,
+  profile: { profile, loading },
+  auth,
+}) => {
+  console.log(match.params.id);
   useEffect(() => {
-    getProfileById(match.params.id)
-  }, [])
+    getProfileById(match.params.id);
+  }, []);
   return (
-    <div>Profile</div>
-  )
-}
+    <Fragment>
+      {profile === null || loading ? (
+        <Spinner />
+      ) : (<Fragment>
+        <Link to="/profiles" className="btn btn-primary">
+          Back to profiles
+        </Link>
+
+        {auth.isAuthenticated &&
+          loading === false &&
+          auth.user._id === profile.user._id && (
+            <Link to="/edit-profile" className="btn btn-primary">
+              Edit User
+            </Link>
+          )}
+
+        </Fragment>
+      )}
+    </Fragment>
+  );
+};
 
 Profile.propTypes = {
   getProfileById: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
-  profile: PropTypes.object.isRequired
-}
+  profile: PropTypes.object.isRequired,
+};
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   auth: state.auth,
-  profile: state.profile
-})
+  profile: state.profile,
+});
 
-export default connect(mapStateToProps, {getProfileById})(Profile)
+export default connect(mapStateToProps, { getProfileById })(Profile);
